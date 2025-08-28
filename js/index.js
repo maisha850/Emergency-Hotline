9622fed9554cf7db9b4b2f527bca08fcc285ba5c
maisha850 <tasnimmaisha08@gmail.com>
2025-08-28 12:31:32 +0600

    fifth commit

--- js/index.js
+++ js/index.js
@@ -22,29 +22,36 @@ for(let callbtn of callbtns){
         const title=callbtn.parentNode.parentNode.children[1].children[0].innerText
         const subtitle=callbtn.parentNode.parentNode.children[1].children[1].innerText
         const number=callbtn.parentNode.parentNode.children[1].children[2].innerText   
-        console.log(title)
-        if(number>0){
-            alert(`📞Calling ${subtitle} ${number} `)
-        }
-        else{
-       alert('❌ You have no sufficient coins.You need at least 20 coins to make a call')
-            
-        }
-        const iconsNum=getElement('coins').innerText
+          const iconsNum=getElement('coins').innerText
         const reduceCoins=Number(iconsNum)-20 
         getElement('coins').innerText=reduceCoins
 
-        const callhistoryContainer=getElement('call-historys')
+        if(iconsNum>0){
+            alert(`📞Calling ${subtitle} ${number} `)
+             const callhistoryContainer=getElement('call-historys')
         const div=document.createElement('div')
         div.innerHTML=`
-        <div class="w-[350px] bg-[#FAFAFA] rounded-lg p-4 shadow-md mb-4"> 
+        <div class="w-full bg-[#FAFAFA] rounded-lg p-4 shadow-md mb-4"> 
         <h3 class="font-semibold text-lg">${title}</h3>
         <p class="text-gray-600 text-lg" >${number}</p>
         </div>`
         callhistoryContainer.appendChild(div)
+        }
+        else{
+       alert('❌ You have no sufficient coins.You need at least 20 coins to make a call')
+       getElement('coins').innerText=0
+
+            
+        }
 
 
-
-
+    })
+}
+const copyBtns=document.getElementsByClassName('copy')
+for(let copyBtn of copyBtns){
+    copyBtn.addEventListener('click',function(){
+        const copy=getElement('copy-btn').innerText
+        const copyNum=Number(copy)+1
+        getElement('copy-btn').innerText=copyNum
     })
 }
